242a38b89e6e5bb5ffe5e9b0413801e7a5028f2d
Norbert Preining <norbert@preining.info>
2015-02-16 11:19:50 +0900

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
 # A modification of Dean Clatworthy's deploy script as found here: https://github.com/deanc/wordpress-plugin-git-svn
-# The difference is that this script lives in the plugin's git repo & doesn't require an existing SVN repo.
+# plus some other script
+#
+# changes by NP
+# 20150216
+# * use rsync to add/remove files when syncing from git to svn
+# * use checkedout version of svn
+
+# error out in case of problems
+set -e
 
 # main config
 PLUGINSLUG="piwigopress"
@@ -11,8 +19,8 @@ MAINFILE="piwigopress.php" # this should be the name of your main php file in th
 GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
 
 # svn config
-SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
-SVNURL="http://plugins.svn.wordpress.org/piwigopress/" # Remote SVN repo on wordpress.org, with no trailing slash
+SVNPATH="$CURRENTDIR/../${PLUGINSLUG}-wordpress.svn" # path to a temp SVN repo. No trailing slash required and don't add trunk.
+SVNURL="http://plugins.svn.wordpress.org/${PLUGINSLUG}/" # Remote SVN repo on wordpress.org, with no trailing slash
 SVNUSER="norbusan" # your svn username
 
 
@@ -30,6 +38,22 @@ then
 	echo "You'll need to install subversion before proceeding. Exiting....";
 	exit 1;
 fi
+
+# We are using rsync to delete unused files
+if [ $(dpkg-query -W -f='${Status}' rsync 2>/dev/null | grep -c "ok installed") != "1" ]
+then
+	echo "You'll need to install rsync before proceeding. Exiting....";
+	exit 1;
+fi
+
+# make sure that there are no uncommitted changes in he subversion repo
+if [ "`svn status $SVNPATH`" != "" ] 
+then
+	echo "svn repo in $SVNPATH is not clean, first commit/revert changes there. Exiting ..."
+	exit 1
+fi
+
+
 
 # Check version in readme.txt is the same as plugin file after translating both to unix line breaks to work around grep's failure to identify mac line breaks
 NEWVERSION1=`grep "^Stable tag:" $GITPATH/readme.txt | awk -F' ' '{print $NF}'`
@@ -55,27 +79,28 @@ read COMMITMSG
 git commit -am "$COMMITMSG"
 
 echo "Tagging new version in git"
-git tag -a "$NEWVERSION1" -m "Tagging version $NEWVERSION1"
+git tag -a "$NEWVERSION1" -s -m "Tagging version $NEWVERSION1"
 
 echo "Pushing latest commit to origin, with tags"
 git push origin master
 git push origin master --tags
 
-echo 
-echo "Creating local copy of SVN repo ..."
-svn co $SVNURL $SVNPATH
+tmpd=`mktemp -d`
+echo "Exporting the HEAD of master from git to temp direcory $tmpd"
+# don't forget the trailing slash!!!
+git checkout-index -a -f --prefix=$tmpd/
 
-echo "Clearing svn repo so we can overwrite it"
-svn rm $SVNPATH/trunk/*
+echo "Updating svn repository"
+svn up $SVNPATH
 
-echo "Exporting the HEAD of master from git to the trunk of SVN"
-git checkout-index -a -f --prefix=$SVNPATH/trunk/
+echo "syncing temp directory into trunk"
+rsync -av --delete $tmpd/ $SVNPATH/trunk/
 
-echo "Ignoring github specific files and deployment script"
-svn propset svn:ignore "deploy.sh
-README.md
-.git
-.gitignore" "$SVNPATH/trunk/"
+#echo "Ignoring github specific files and deployment script"
+#svn propset svn:ignore "deploy.sh
+#README.md
+#.git
+#.gitignore" "$SVNPATH/trunk/"
 
 echo "Changing directory to SVN and committing to trunk"
 cd $SVNPATH/trunk/
@@ -89,7 +114,11 @@ svn copy trunk/ tags/$NEWVERSION1/
 cd $SVNPATH/tags/$NEWVERSION1
 svn commit --username=$SVNUSER -m "Tagging version $NEWVERSION1"
 
-echo "Removing temporary directory $SVNPATH"
-rm -fr $SVNPATH/
+
+echo "Removing temp dir"
+if [ -d ${tmpd} ]
+then
+	rm -rf $tmpd
+fi
 
 echo "*** FIN ***"
